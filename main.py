b1224223f98f4a709c3b2588dde840271497dce9
Pedro <pcastillotorio@gmail.com>
2021-12-07 21:55:39 +0100

    Mejora de interfaz
    Solo se cargan datos cuando el usuario lo especifica
    Se tiene que seleccionar un importe minimo y maximo para la visualizacion del mapa
    No aparece importe maximo hasta que el minimo esta especificado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import _thread
+import time
 
 import numpy
 
@@ -73,154 +74,173 @@ def load_data(nFilas):
 # Input para obtener las filas a cargar
 nFilas = st.number_input("Número de filas a cargar: ", 0, 890612, step=1)
 dataCards = []
+if nFilas:
+    data_load_state = st.text('Cargando datos...')
+    dataCards, dataWeather = load_data(nFilas)
+    if not dataCards.cache():
+        dataCards.cache()
+        dataCards.count()
 
-data_load_state = st.text('Cargando datos...')
-dataCards, dataWeather = load_data(nFilas)
-data_load_state.text("Completado!")
+    if not dataWeather.cache():
+        dataWeather.cache()
+        dataWeather.count()
 
-if st.checkbox('Mostrar datos'):
-    st.subheader('Datos')
-    st.write(dataCards.toPandas())
 
-st.subheader('Numero de compras por franja horaria')
-# Obtenemos las compras que hay por cada franja horaria y creamos un DataFrame para mostrarlo en el gráfico
 
-list_franja = []
-for col in dataCards.collect():
-    list_franja.append(col["franja_horaria"])
+    data_load_state.text("Completado!")
 
-counts = Counter(list_franja)
-df_horas_compra = pd.DataFrame.from_dict(counts, orient='index')
-st.bar_chart(df_horas_compra)
+    if st.checkbox('Mostrar datos'):
+        st.subheader('Datos')
+        st.write(dataCards.toPandas())
 
-# Preparamos el titulo y cargamos los datos en el mapa
-st.subheader('Mapa de compras')
+    st.subheader('Numero de compras por franja horaria')
+    # Obtenemos las compras que hay por cada franja horaria y creamos un DataFrame para mostrarlo en el gráfico
 
-# Creamos un slider para filtrar los resultados en el mapa
-hour_to_filter = st.slider('Hora', 0, 22, 16, step=2)
+    list_franja = []
+    for col in dataCards.collect():
+        list_franja.append(col["franja_horaria"])
 
-# Como nos devuelve un entero, lo hacemos string y cambiamos para que pueda compararse con los datos del DataFrame
-# En caso de ser un numero de un solo digito, lo añadimos un 0 al principio para coincidir con el formato del DataFrame
-if re.match(r'^\d$', str(hour_to_filter)) is None:
-    hour_to_filter = str(hour_to_filter) + '-' + str(hour_to_filter+2)
-else:
-    if hour_to_filter + 2 < 10:
-        hour_to_filter = '0' + str(hour_to_filter) + '-0' + str(hour_to_filter+2)
+    counts = Counter(list_franja)
+    df_horas_compra = pd.DataFrame.from_dict(counts, orient='index')
+    st.bar_chart(df_horas_compra)
+
+    # Preparamos el titulo y cargamos los datos en el mapa
+    st.subheader('Mapa de compras')
+
+    # Creamos un slider para filtrar los resultados en el mapa
+    hour_to_filter = st.slider('Hora', 0, 22, 16, step=2)
+
+    # Como nos devuelve un entero, lo hacemos string y cambiamos para que pueda compararse con los datos del DataFrame
+    # En caso de ser un numero de un solo digito, lo añadimos un 0 al principio para coincidir con el formato del DataFrame
+    if re.match(r'^\d$', str(hour_to_filter)) is None:
+        hour_to_filter = str(hour_to_filter) + '-' + str(hour_to_filter+2)
     else:
-        hour_to_filter = '0' + str(hour_to_filter) + '-' + str(hour_to_filter+2)
+        if hour_to_filter + 2 < 10:
+            hour_to_filter = '0' + str(hour_to_filter) + '-0' + str(hour_to_filter+2)
+        else:
+            hour_to_filter = '0' + str(hour_to_filter) + '-' + str(hour_to_filter+2)
 
-#Importe maximo del dataset
-importeMaxData = int(dataCards.agg({'importe': 'max'}).collect()[0]['max(importe)'])
+    #Importe maximo del dataset
+    importeMaxData = int(dataCards.agg({'importe': 'max'}).collect()[0]['max(importe)'])
 
-#Inputs importe
-importeMin = int(st.number_input("Importe mínimo: ", 0, importeMaxData, step=1, value=20))
-importeMax = int(st.number_input("Importe máximo: ", importeMin, importeMaxData, step=1, value=50))
+    importe_to_filter = [None, None]
+    if st.checkbox('Utilizar slider'):
+        # Creamos un slider para filtrar por importe en el mapa
+        importe_to_filter = st.slider('Importe', 0, importeMaxData, (0, 50), step=1)
 
-# Creamos un slider para filtrar por importe en el mapa
-importe_to_filter = st.slider('Importe', 0, importeMaxData, (importeMin, importeMax), step=1)
-filtered_data = dataCards.select('lat', 'lon', 'lat_cliente', 'lon_cliente', 'franja_horaria')\
-    .filter(
-        (dataCards.franja_horaria == hour_to_filter) &
-        (dataCards.importe > importe_to_filter[0]) &
-        (dataCards.importe < importe_to_filter[1])
-    ).toPandas()
+    else:
 
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-    latitude=36.838139,
-    longitude=-2.459740,
-    zoom=8,
-    pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            "ArcLayer",
-            data=filtered_data,
-            get_source_position=["lon_cliente", "lat_cliente"],
-            get_target_position=["lon", "lat"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-        pdk.Layer(
-            "HexagonLayer",
-            data=filtered_data,
-            get_position=["lon_cliente", "lat_cliente"],
-            radius=2000,
-            elevation_scale=400,
-            elevation_range=[0, 600],
-            extruded=True,
-        ),
-    ],
-))
+        #Inputs importe
+        importe_to_filter[0] = int(st.number_input("Importe mínimo: ", 0, importeMaxData, step=1))
+        if importe_to_filter[0]:
+            importe_to_filter[1] = int(st.number_input("Importe máximo: ", importe_to_filter[0], importeMaxData, step=1))
 
-# KPI compras por mes representado en barras
-st.subheader("Compras por mes")
-df_mes = dataCards
-df_mes = df_mes.withColumn('dia', substring('dia', 1, 7))
-# df_mes = spark.createDataFrame(get_mes, df_mes.schema)
-list_mes = []
-for col in df_mes.collect():
-    list_mes.append(col["dia"])
-counts_mes = Counter(list_mes)
-df_mes_compra = pd.DataFrame.from_dict(counts_mes, orient='index')
-st.caption('Numero de compras por mes.')
-df_mes_compra.columns = ['nCompras']
-st.bar_chart(df_mes_compra)
+    if importe_to_filter[1]:
+        filtered_data = dataCards.select('lat', 'lon', 'lat_cliente', 'lon_cliente', 'franja_horaria')\
+            .filter(
+                (dataCards.franja_horaria == hour_to_filter) &
+                (dataCards.importe > importe_to_filter[0]) &
+                (dataCards.importe < importe_to_filter[1])
+            ).toPandas()
 
-# Weather por mes
-weather_mes = dataWeather
-weather_mes = weather_mes.withColumn('FECHA', substring('FECHA', 1, 7))
-weather_mes = weather_mes.groupby('FECHA').agg(avg('TMax'), avg('TMed'), avg('TMin'))
-weather_mes = weather_mes.toPandas()
-weather_mes = weather_mes.set_index('FECHA')
-st.caption('Temperatura por mes.')
-st.line_chart(weather_mes)
+        st.write('Rango de horas:   ', hour_to_filter)
+        st.write('Rango de importe: ', importe_to_filter[0], '-', importe_to_filter[1])
+        st.pydeck_chart(pdk.Deck(
+            map_style='mapbox://styles/mapbox/light-v9',
+            initial_view_state=pdk.ViewState(
+            latitude=36.838139,
+            longitude=-2.459740,
+            zoom=8,
+            pitch=50,
+            ),
+            layers=[
+                pdk.Layer(
+                    "ArcLayer",
+                    data=filtered_data,
+                    get_source_position=["lon_cliente", "lat_cliente"],
+                    get_target_position=["lon", "lat"],
+                    get_source_color=[200, 30, 0, 160],
+                    get_target_color=[200, 30, 0, 160],
+                    auto_highlight=True,
+                    width_scale=0.0001,
+                    get_width="outbound",
+                    width_min_pixels=3,
+                    width_max_pixels=30,
+                ),
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=filtered_data,
+                    get_position=["lon_cliente", "lat_cliente"],
+                    radius=2000,
+                    elevation_scale=400,
+                    elevation_range=[0, 600],
+                    extruded=True,
+                ),
+            ],
+        ))
 
-# KPI dinero gastado al mes
-df_gastos_mes = df_mes.select('dia', 'importe')
-df_gastos_mes = df_gastos_mes.groupby('dia').sum('importe')
-st.caption('Importe gastado por mes.')
-df_gastos_mes = df_gastos_mes.toPandas()
-df_gastos_mes = df_gastos_mes.set_index('dia')
-st.area_chart(df_gastos_mes)
+    # KPI compras por mes representado en barras
+    st.subheader("Compras por mes")
+    df_mes = dataCards
+    df_mes = df_mes.withColumn('dia', substring('dia', 1, 7))
+    # df_mes = spark.createDataFrame(get_mes, df_mes.schema)
+    list_mes = []
+    for col in df_mes.collect():
+        list_mes.append(col["dia"])
+    counts_mes = Counter(list_mes)
+    df_mes_compra = pd.DataFrame.from_dict(counts_mes, orient='index')
+    st.caption('Numero de compras por mes.')
+    df_mes_compra.columns = ['nCompras']
+    st.bar_chart(df_mes_compra)
 
-# Compras por dias del mes
-st.subheader('Estadísticas por dias del mes')
-mes = st.select_slider(
-     'Selecciona el mes',
-     options=DICT_MES.keys(),
-     value='Enero'
-)
+    # Weather por mes
+    weather_mes = dataWeather
+    weather_mes = weather_mes.withColumn('FECHA', substring('FECHA', 1, 7))
+    weather_mes = weather_mes.groupby('FECHA').agg(avg('TMax'), avg('TMed'), avg('TMin'))
+    weather_mes = weather_mes.toPandas()
+    weather_mes = weather_mes.set_index('FECHA')
+    st.caption('Temperatura por mes.')
+    st.line_chart(weather_mes)
 
-# Numero de compras por dia
-df_dias_mes = dataCards.select('dia')
-df_dias_mes = df_dias_mes.filter(dataCards.dia.startswith(DICT_MES[mes]))
-list_dias_mes = [row[0] for row in df_dias_mes.select('dia').collect()]
-counts_dias = Counter(list_dias_mes)
-df_compras_dias_mes = pd.DataFrame.from_dict(counts_dias, orient='index')
-df_compras_dias_mes.columns = ['nCompras']
-st.caption('Numero de compras por dia del mes.')
-st.bar_chart(df_compras_dias_mes)
+    # KPI dinero gastado al mes
+    df_gastos_mes = df_mes.select('dia', 'importe')
+    df_gastos_mes = df_gastos_mes.groupby('dia').sum('importe')
+    st.caption('Importe gastado por mes.')
+    df_gastos_mes = df_gastos_mes.toPandas()
+    df_gastos_mes = df_gastos_mes.set_index('dia')
+    st.area_chart(df_gastos_mes)
 
-# Temperatura por dia
-df_temp_dia = dataWeather.select('FECHA', 'TMax', 'TMed', 'TMin')
-df_temp_dia = df_temp_dia.filter(df_temp_dia.FECHA.startswith(DICT_MES[mes]))
-df_temp_dia = df_temp_dia.toPandas().set_index('FECHA')
-st.caption('Temperatura por dia del mes.')
-st.line_chart(df_temp_dia)
+    # Compras por dias del mes
+    st.subheader('Estadísticas por dias del mes')
+    mes = st.select_slider(
+         'Selecciona el mes',
+         options=DICT_MES.keys(),
+         value='Enero'
+    )
 
-# Precipitaciones por dia
-df_prec_dia = dataWeather.select('FECHA', 'Precip')
-df_prec_dia = df_prec_dia.filter(df_prec_dia.FECHA.startswith(DICT_MES[mes]))
-df_prec_dia = df_prec_dia.toPandas().set_index('FECHA')
-st.caption('Precipitaciones por dia del mes.')
-st.line_chart(df_prec_dia)
+    # Numero de compras por dia
+    df_dias_mes = dataCards.select('dia')
+    df_dias_mes = df_dias_mes.filter(dataCards.dia.startswith(DICT_MES[mes]))
+    list_dias_mes = [row[0] for row in df_dias_mes.select('dia').collect()]
+    counts_dias = Counter(list_dias_mes)
+    df_compras_dias_mes = pd.DataFrame.from_dict(counts_dias, orient='index')
+    df_compras_dias_mes.columns = ['nCompras']
+    st.caption('Numero de compras por dia del mes.')
+    st.bar_chart(df_compras_dias_mes)
+
+    # Temperatura por dia
+    df_temp_dia = dataWeather.select('FECHA', 'TMax', 'TMed', 'TMin')
+    df_temp_dia = df_temp_dia.filter(df_temp_dia.FECHA.startswith(DICT_MES[mes]))
+    df_temp_dia = df_temp_dia.toPandas().set_index('FECHA')
+    st.caption('Temperatura por dia del mes.')
+    st.line_chart(df_temp_dia)
+
+    # Precipitaciones por dia
+    df_prec_dia = dataWeather.select('FECHA', 'Precip')
+    df_prec_dia = df_prec_dia.filter(df_prec_dia.FECHA.startswith(DICT_MES[mes]))
+    df_prec_dia = df_prec_dia.toPandas().set_index('FECHA')
+    st.caption('Precipitaciones por dia del mes.')
+    st.line_chart(df_prec_dia)
 
 
 
